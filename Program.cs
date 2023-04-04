40f950ff8daaa4fed5a38547de286b7cf0ad55e6
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-04 20:25:47 +0700

    Prime

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            bool ok = true;
+            if(n < 2)
+            {
+                Console.WriteLine(n + " is not a Prime");
+                return;
+            }
+
+            while(i++ <= Math.Sqrt(n))
+            {
+                if(n % i == 0)
+                {
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok == true)
+            {
+                Console.WriteLine(n + " is a Prime");
+            }
+            else Console.WriteLine(n + " is not a Prime");
+        }
+    }
+}
